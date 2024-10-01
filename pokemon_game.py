c276f935ddbb5ddef9de8a16c8ca42f855c602d7
Carmen <carmenrvg@gmail.com>
2024-10-01 09:33:01 +0100

    Carmen cpu ramdom

--- pokemon_game.py
+++ pokemon_game.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import random
 
 # Get the list of pokemon from the API
 url = 'https://pokeapi.co/api/v2/pokemon/'
@@ -14,6 +15,8 @@ print('Enter your pokemon:')
 
 # Get the user's choice
 choice = input().lower()
+my_pokemon = choice
+print("My pokemon is", my_pokemon)
 
 # Get the pokemon's data from the API
 url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(choice)
@@ -36,3 +39,32 @@ print('Name: {}'.format(pokemon_data['name']))
 print('Weight: {}'.format(weight_formatted) + "(kgs)")
 print('Height: {}'.format(height_formatted) + "(m)")
 print('Ability: {}'.format(ability['name']))
+
+print(my_pokemon)
+
+#pokemon of cpu>Random select
+cpu_pokemon_id = random.randint(1, 151)
+url = f'https://pokeapi.co/api/v2/pokemon/{cpu_pokemon_id}/'
+response = requests.get(url)
+cpu_pokemon_data = json.loads(response.text)
+
+# Extract height, weight, hp and attack for CPU
+# to get ability
+abilities = cpu_pokemon_data['abilities'][0]
+cpu_ability = abilities['ability']
+
+# to format height and weight properly
+cpu_height = int(cpu_pokemon_data['height'])
+cpu_weight = int(cpu_pokemon_data['weight'])
+
+cpu_height_formatted = height / 10
+cpu_weight_formatted = weight / 10
+
+# Print the pokemon's data
+print('Name cpu pokemon: {}'.format(cpu_pokemon_data['name']))
+print('Weight: {}'.format(cpu_weight_formatted) + "(kgs)")
+print('Height: {}'.format(cpu_height_formatted) + "(m)")
+print('Ability: {}'.format(cpu_ability['name']))
+
+# Print the initial battle message showing both Pokémon
+print(f"Your {pokemon_data['name']} VS my {cpu_pokemon_data['name']}! Start the fight!")
