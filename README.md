8453f704f112bcdb3574d3d297e93e49c1ace6f8
NAO <anjyogunwoolu@gmail.com>
2024-10-01 09:30:26 +0100

    updated nao game attempt

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Group_pokemon_game
 group 3
+
+Guidance:
+
+* Work in your groups of 2-3 to make a single game
+* API documentation: PokéAPI (pokeapi.co)
+* Recommended: Understand the Pokemon lookup program first (code below) to fully understand accessing data from the API.
+* The Pokémon data MUST come from the PokeApi
+* Get random Pokémon for at least the CPU (Player can be chosen or random)
+* Pokémon should fight and a winner should be declared in some way
+* No Pygame. Focus on interacting with the API.
+* Be as creative as you like after this. Can you incorporate different abilities/stats etc.?
+* Try and work collaboratively on the one repo using Git
+* To deliver: Give it your best shot! Share your code (message your Ramon + Luke directly, NOT a message in the main chat) by COB (17:00)
+
+User Stories:
+* as a player i want get a random pokemon to play as CPU so each battle is with pokemon
+* as a player i want to be able to choose my pokemon so i can battle with the CPU
+* as a player i want to have a winner declared after the battle so i can know if my chosen pokemon won
+
+*as a player i want to be able to see stats of my chose pokemon before i battle so that i can choose the right pokemon for the battle
+
--- pokemon_game.py
+++ pokemon_game.py
@@ -1,23 +1,42 @@
 import requests
 import json
+import random
+
 
 # Get the list of pokemon from the API
-url = 'https://pokeapi.co/api/v2/pokemon/'
-response = requests.get(url)
-pokemon_list = json.loads(response.text)['results']
+#url = 'https://pokeapi.co/api/v2/pokemon/'
+#response = requests.get(url)
+#pokemon_list = json.loads(response.text)['results']
 
-for pokemon in pokemon_list:
-    print(pokemon['name'])
+def pokemon_data(pokemon):
+    url = 'https://pokeapi.co/api/v2/pokemon/{cpu_pokemon_id}/'
+    response = requests.get(url)
+    return json.loads(response.text)
+
+
+cpu_pokemon_id = random.randint(1, 1302)  # found the count of pokemon IDs possible
+cpu_pokemon_data = pokemon_data(cpu_pokemon_id)
+
+cpu_height = (cpu_pokemon_data['height'])
+cpu_weight = (cpu_pokemon_data['weight'])
+cpu_hp = (cpu_pokemon_data['hp'])
+cpu_attack = (cpu_pokemon_data['attack'])
+
+print(f"CPU chose {cpu_pokemon_data['name']}:")
+print(f'Name: {cpu_pokemon_data["name"]}')
+print(f'Weight: {cpu_weight} kg')
+print(f'Height: {cpu_height} m')
+print(f'HP: {cpu_hp}')
+print(f'Attack: {cpu_attack}')
 
 # Ask the user to choose a pokemon
-print('Enter your pokemon:')
+
 
 # Get the user's choice
 choice = input().lower()
 
 # Get the pokemon's data from the API
-url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(choice)
-response = requests.get(url)
+
 pokemon_data = json.loads(response.text)
 
 # to get ability
@@ -36,3 +55,29 @@ print('Name: {}'.format(pokemon_data['name']))
 print('Weight: {}'.format(weight_formatted) + "(kgs)")
 print('Height: {}'.format(height_formatted) + "(m)")
 print('Ability: {}'.format(ability['name']))
+
+#make the cpu choose a pokemon from random
+
+
+
+cpu_pokemon = pokemon_data['name']
+print(f"Your opponent is {cpu_pokemon}")
+
+player_input = input("Enter your pokemon name: ")
+if player_input == pokemon_data['name']:
+    print(player_input)
+else:
+    player_input = ""
+    player_pokemon_id = cpu_pokemon_id
+
+
+
+
+#response = requests.get(url)  #get a response
+#data_dict = response.json()
+
+#with open('pokemon.json', 'w') as file:
+    #json.dump(data_dict, file)
+
+#print(f"CPU pokemon: {data_dict["name"]}")
+#print(f"{data_dict["name"]} is type {data_dict['type']}")
